d016487e58c0c4f7a84f41652487d833a7aae713
Gowthamen <113452708+Gowthamen@users.noreply.github.com>
2022-10-17 20:04:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+eqn="";press_count=0;pt_flag=1;
+key_list=document.getElementsByClassName("keys");
+function add_user_input(symbol,flag){
+    var_exp_fl=flag;
+    if(press_count==0){
+        if(symbol=="0" || symbol=="00")
+        {pt_flag=1;}
+        else if(var_exp_fl==1){
+        if(pt_flag==1){
+        document.getElementById("input_area").innerHTML+=symbol;pt_flag=0;}press_count++;}
+        else{
+        document.getElementById("input_area").innerHTML=symbol;press_count++;pt_flag=1;}
+    }
+    else{
+    document.getElementById("input_area").innerHTML+=symbol;pt_flag=1;}
+}
+function clear_all(){
+    press_count=0;;
+    document.getElementById("input_area").innerHTML=0;
+    document.getElementById("result_area").innerHTML="" ;
+}
+
+function evaluate_eqn(){
+    if(var_exp_fl==0){
+    eqn=document.getElementById("input_area").innerHTML;press_count=0
+    document.getElementById("result_area").innerHTML=eval(eqn);}
+    else
+    alert("Invalid values given")
+}
